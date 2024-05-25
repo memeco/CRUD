3ba7d1de56a7997d4c4385b4140ee341690d354b
Lucas Quadros <olucasquadros@gmail.com>
2024-05-24 21:44:55 -0300

    create

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Crud
 {
@@ -7,104 +6,28 @@ namespace Crud
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.WriteLine("Escolha uma opção:");
-                Console.WriteLine("1 - Criar uma pessoa");
-                Console.WriteLine("2 - Ler pessoas");
-                Console.WriteLine("3 - Editar uma pessoa");
-                Console.WriteLine("4 - Excluir uma pessoa");
-                Console.WriteLine("5 - Sair");
-
-                string opcao = Console.ReadLine();
-
-                switch (opcao)
-                {
-                    case "1":
-                        CriarPessoa();
-                        break;
-                    case "2":
-                        LerPessoas();
-                        break;
-                    case "3":
-                        EditarPessoa();
-                        break;
-                    case "4":
-                        ExcluirPessoa();
-                        break;
-                    case "5":
-                        return;
-                    default:
-                        Console.WriteLine("Opção inválida. Tente novamente.");
-                        break;
-                }
-            }
-        }
-
-        static void CriarPessoa()
-        {
-            Console.WriteLine("Digite o ID:");
+            // Perguntando os valores ao usuário
+            Console.WriteLine("Digite o ID: ");
             int id = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite o Nome:");
+            Console.WriteLine("Digite o nome: ");
             string nome = Console.ReadLine();
 
-            Console.WriteLine("Digite a Idade:");
+            Console.WriteLine("Digite a idade: ");
             int idade = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite a Altura:");
+            Console.WriteLine("Digite a altura (em metros): ");
             double altura = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite o Peso:");
+            Console.WriteLine("Digite o peso (em kg): ");
             double peso = double.Parse(Console.ReadLine());
 
+            // Criando um objeto da classe Pessoa
             Pessoa pessoa = new Pessoa(id, nome, idade, altura, peso);
+
+            // Salvando o objeto no banco de dados
             pessoa.Criar();
-
-            Console.WriteLine("Pessoa criada com sucesso!");
-        }
-
-        static void LerPessoas()
-        {
-            List<Pessoa> pessoas = Pessoa.Ler();
-
-            foreach (var pessoa in pessoas)
-            {
-                Console.WriteLine($"ID: {pessoa.Id}, Nome: {pessoa.Nome}, Idade: {pessoa.Idade}, Altura: {pessoa.Altura}, Peso: {pessoa.Peso}");
-            }
-        }
-
-        static void EditarPessoa()
-        {
-            Console.WriteLine("Digite o ID da pessoa a ser editada:");
-            int id = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o novo Nome:");
-            string nome = Console.ReadLine();
-
-            Console.WriteLine("Digite a nova Idade:");
-            int idade = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite a nova Altura:");
-            double altura = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o novo Peso:");
-            double peso = double.Parse(Console.ReadLine());
-
-            Pessoa pessoa = new Pessoa(id, nome, idade, altura, peso);
-            pessoa.Editar();
-
-            Console.WriteLine("Pessoa editada com sucesso!");
-        }
-
-        static void ExcluirPessoa()
-        {
-            Console.WriteLine("Digite o ID da pessoa a ser excluída:");
-            int id = int.Parse(Console.ReadLine());
-
-            Pessoa.Excluir(id);
-
-            Console.WriteLine("Pessoa excluída com sucesso!");
+            Console.WriteLine("Pessoa cadastrada!");
         }
     }
 }
--- obj/Debug/net8.0/Crud.AssemblyInfo.cs
+++ obj/Debug/net8.0/Crud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea5c913c5b653360834bea36de141faec910428")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff9b8ac582c580a5ac21854dc9f45f9feeb1dee5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
